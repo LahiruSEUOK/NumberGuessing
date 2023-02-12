7c1be4886ed3caf5e8326b3a3ed049356cc7fb28
Lahiru Ekanayaka <48405541+LahiruSEUOK@users.noreply.github.com>
2023-02-12 23:42:25 +0530

    done

--- src/Playgame.java
+++ src/Playgame.java
@@ -10,36 +10,37 @@ public class Playgame {
    public void playgame() throws Exception{
 
     System.out.println("Number is between 1-11 \nEnter your guess");
-    Scanner scan01 = new Scanner(System.in);
+    try (Scanner scan01 = new Scanner(System.in)) {
+        for(int i=1; i<=5; i++){
+            int userinput = scan01.nextInt();
+            if (userinput==startnumber){
+                System.out.println("YOU WON");
+                
+            }
+            else if(i==5){
+                System.out.println("You failed");
+                System.out.println("Type \'1\' to Restrat the Game");
+                try (Scanner scan02 = new Scanner(System.in)) {
+                    int restartinput = scan02.nextInt();
+                    int expectedinputtorestart = 1;
 
-    for(int i=1; i<=5; i++){
-        int userinput = scan01.nextInt();
-        if (userinput==startnumber){
-            System.out.println("YOU WON");
-            
-        }
-        else if(i==5){
-            System.out.println("You failed");
-            System.out.println("Type \'1\' to Restrat the Game");
-            Scanner scan02 = new Scanner(System.in);
-            int restartinput = scan02.nextInt();
-            int expectedinputtorestart = 1;
-
-            if(restartinput==expectedinputtorestart){
-                System.out.println("Finishes");
-                App restart = new App();
-                restart.main(null);;
+                    if(restartinput==expectedinputtorestart){
+                        System.out.println("Finishes");
+                        new App();
+                        App.main(null);;
+                    }
+                    else{
+                         System.out.println(expectedinputtorestart);
+                         System.out.println(restartinput);
+                         System.out.println("GAME OVER");                
+                     }
+                }
             }
             else{
-                 System.out.println(expectedinputtorestart);
-                 System.out.println(restartinput);
-                 System.out.println("GAME OVER");                
-             }
-        }
-        else{
-            System.out.println(" YOU HAVE ONLY "+ (5-i) +" CHANSES");
-        }
+                System.out.println(" YOU HAVE ONLY "+ (5-i) +" CHANSES");
+            }
 
+        }
     }
 
 
