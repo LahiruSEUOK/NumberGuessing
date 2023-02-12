4271ce5feb835d196685ecdb6b8fd04bcef83bff
Lahiru Ekanayaka <48405541+LahiruSEUOK@users.noreply.github.com>
2023-02-12 23:34:55 +0530

    Project finished on the same day.

---
+++ src/Playgame.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Playgame {
+ 
+   StartGame gamestartnumber = new StartGame();
+   int startnumber = gamestartnumber.startgame();
+
+   int chanses = 5;
+   
+   public void playgame() throws Exception{
+
+    System.out.println("Number is between 1-11 \nEnter your guess");
+    Scanner scan01 = new Scanner(System.in);
+
+    for(int i=1; i<=5; i++){
+        int userinput = scan01.nextInt();
+        if (userinput==startnumber){
+            System.out.println("YOU WON");
+            
+        }
+        else if(i==5){
+            System.out.println("You failed");
+            System.out.println("Type \'1\' to Restrat the Game");
+            Scanner scan02 = new Scanner(System.in);
+            int restartinput = scan02.nextInt();
+            int expectedinputtorestart = 1;
+
+            if(restartinput==expectedinputtorestart){
+                System.out.println("Finishes");
+                App restart = new App();
+                restart.main(null);;
+            }
+            else{
+                 System.out.println(expectedinputtorestart);
+                 System.out.println(restartinput);
+                 System.out.println("GAME OVER");                
+             }
+        }
+        else{
+            System.out.println(" YOU HAVE ONLY "+ (5-i) +" CHANSES");
+        }
+
+    }
+
+
+   
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+   }
+
+
+}
